4a5c4283a6be8970c73be7e4c7d174f6a86ae1a4
tgray017 <tgray017@gmail.com>
2018-10-17 01:15:27 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -30,7 +30,7 @@ class Song
   end
   
   def self.find_by_name(name)
-    self.all.detect {|song| song == name}
+    self.all.detect {|song| song.name == name}
   end
   
   def self.find_or_create_by_name(name)
