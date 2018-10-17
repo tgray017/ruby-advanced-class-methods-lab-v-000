eccdbf20e93ff98aba8d4a0f64d1d43570797ed3
tgray017 <tgray017@gmail.com>
2018-10-17 01:23:25 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -45,7 +45,7 @@ class Song
     self.all.sort_by {|song| song.name} 
   end
   
-  def self.new_from_filename
-    
+  def self.new_from_filename(artist_and_song)
+    artist_and_song.split!('-')
   end
 end
